ebbde5f12cc0576bffc81dab47b5d1ad03ed992c
縫嶋慧深 <tefu@tefuko.local>
2020-02-20 12:35:26 +0900

    count

---
+++ Tefu/wc.py
@@ -0,0 +1,31 @@
+# Janomeのインストール
+# pip install janome | tail -n 1
+# ターミナルで実行
+
+# Janomeのロード
+from janome.tokenizer import Tokenizer
+
+path = "data/kusamakura.txt"
+with open(path) as text :
+    text = text.read()
+
+# ファイル整形
+import re
+
+# 注釈の除去
+if "※" in text :
+    for i in range(text.count("※")) :
+        s = int(text.find("※"))
+        g = int(text.find(")"))
+        text = text[:s] + text[g + 1:]
+
+line = text.count("\n")
+
+# 空行の削除
+text = re.sub("\n", "", text)
+text = re.sub("\r", "", text)
+text = re.sub("\u3000", "", text)
+
+word = len(text)
+
+print(line, word)
