f565f89cf775b49fad818669a3b0f4c3420523d1
fuzi2mo8 <fuzimoto.192128@icloud.com>
2020-02-20 17:08:40 +0900

    kadai5

--- Fuji/mecab.py
+++ Fuji/mecab.py
@@ -1,12 +1,15 @@
 import MeCab
+import csv
 
 mecab = MeCab.Tagger ('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
 
 text = open('/Users/fujiwara/Git/PythonNLP/data/kusamakura.txt').read()
 mecab.parse('')#文字列がGCされるのを防ぐ
 node = mecab.parseToNode(text)
+
 hinnsi = 0
 meisi = 0
+
 while node:
     #単語を取得
     word = node.surface
@@ -14,9 +17,22 @@ while node:
     pos = node.feature.split(",")[1]
     hinnsi += 1
     if node.feature.split(",")[0] == "名詞":
+        #print('{0} , {1}'.format(word, pos))
         meisi += 1
     #次の単語に進める
     node = node.next
+
 print("名詞:"+str(meisi))
 print("全単語:"+str(hinnsi))
-print(str(meisi/hinnsi*100))
+hinndo = meisi/hinnsi*100
+print(str(hinndo))
+
+f = open('result.csv','w')
+writer = csv.writer(f, lineterminator='\n')
+
+csvlist = []
+csvlist.append(hinndo)
+
+writer.writerow(csvlist)
+
+f.close()
