c4ce54b8aa69a9c93d41c4ce4fa1696f049d954a
Emi <sakura.math.sashimo@gmail.com>
2020-02-20 16:05:19 +0900

    counter

---
+++ Tefu/mecab_analysis.py
@@ -0,0 +1,25 @@
+import MeCab
+import wc
+from collections import Counter
+
+text = wc.text
+
+mecab = MeCab.Tagger ('/usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+result = mecab.parse(text)
+
+lines = result.split("\n")
+words = []
+
+for line in lines :
+    feature = line.split("\t")
+    if len(feature) == 2 :
+        info = feature[1].split(",")
+        if info[0] == "名詞" :
+            words.append(info[6])
+
+cnt = Counter(words)
+set = []
+
+for w in cnt.items() :
+    set.append(cnt)
+
