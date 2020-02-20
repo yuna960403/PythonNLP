b7b2bf41bfc680a2e87e1d761127bfcff1ae17e5
fuzi2mo8 <fuzimoto.192128@icloud.com>
2020-02-20 16:36:04 +0900

    mecab-2

--- Fuji/mecab.py
+++ Fuji/mecab.py
@@ -1,10 +1,22 @@
-# -*- coding: utf-8 -*-
-
-
-
 import MeCab
-#辞書を指定
-mecab = MeCab.Tagger("-Owakati -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
-mecab.parse("東京スカイツリ-")
 
-#'東京スカイツリー \n'
+mecab = MeCab.Tagger ('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+
+text = open('/Users/fujiwara/Git/PythonNLP/data/kusamakura.txt').read()
+mecab.parse('')#文字列がGCされるのを防ぐ
+node = mecab.parseToNode(text)
+hinnsi = 0
+meisi = 0
+while node:
+    #単語を取得
+    word = node.surface
+    #品詞を取得
+    pos = node.feature.split(",")[1]
+    hinnsi += 1
+    if node.feature.split(",")[0] == "名詞":
+        meisi += 1
+    #次の単語に進める
+    node = node.next
+print("名詞:"+str(meisi))
+print("全単語:"+str(hinnsi))
+print(str(meisi/hinnsi*100))
